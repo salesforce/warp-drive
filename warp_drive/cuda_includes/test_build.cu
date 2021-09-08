0e87c4e5de9c9247d604c7ad0fd3ed831008c88c
Tian Lan <tian.lan@tianlan-ltm.internal.salesforce.com>
2021-09-07 22:38:30 -0700

    unittest external call

--- warp_drive/cuda_includes/test_build.cu
+++ warp_drive/cuda_includes/test_build.cu
@@ -8,9 +8,10 @@
 #define CUDA_INCLUDES_TEST_BUILD_CONST_H_
 const int num_envs = 2;
 const int num_agents = 5;
-#endif  // CUDA_INCLUDES_TEST_BUILD_CONST_H_
 
 #include "./core_service.h"
 #include "../../example_envs/dummy_env/test_step.cu"
 #include "../../example_envs/tag_gridworld/tag_gridworld_step.cu"
 #include "../../example_envs/tag_continuous/tag_continuous_step.cu"
+
+#endif  // CUDA_INCLUDES_TEST_BUILD_CONST_H_

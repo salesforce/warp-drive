fb836661f81f6f369a73fb3b90ece415bfd7d8b2
Brenton Chu <bchu@nvidia.com>
2022-05-20 12:41:22 -0700

    added numba environment backend support

--- warp_drive/managers/data_manager.py
+++ warp_drive/managers/data_manager.py
@@ -17,16 +17,6 @@ import pycuda.driver as pycuda_driver
 from warp_drive.utils.data_feed import DataFeed
 
 
-class CudaTensorHolder(pycuda.driver.PointerHolderBase):
-    """
-    A class that facilitates casting tensors to pointers.
-    """
-
-    def __init__(self, t):
-        super().__init__()
-        self.gpudata = t.data_ptr()
-
-
 class CUDADataManager:
     """
     CUDA Data Manager: manages the data initialization of GPU,
@@ -469,10 +459,9 @@ class CUDADataManager:
         assert name in self._dtype
         return self._dtype[name]
 
-    @staticmethod
-    def _type_warning_helper(key: str, old: str, new: str, comment=None):
+    def _type_warning_helper(self, key: str, old: str, new: str, comment=None):
         logging.warning(
-            f"CUDADataManager casts the data '{key}' " f"from type {old} to {new}"
+            f"{self.__class__.__name__} casts the data '{key}' " f"from type {old} to {new}"
         )
         if comment:
             logging.warning(comment)
@@ -496,54 +485,3 @@ class CUDADataManager:
     @property
     def log_data_list(self):
         return self._log_data_list
-
-class PyCUDADataManager(CUDADataManager):
-    
-    def pull_data_from_device(self, name: str):
-        
-        assert name in self._host_data
-        if name in self._scalar_data_list:
-            return self._host_data[name]
-
-        if self.is_data_on_device_via_torch(name):
-            return self._device_data_via_torch[name].cpu().numpy()
-
-        assert name in self._device_data_pointer
-
-        v = np.empty_like(self._host_data[name])
-        pycuda_driver.memcpy_dtoh(v, self._device_data_pointer[name])
-        return v
-
-    def reset_device(self, name: Optional[str] = None):
-        if name is not None:
-            assert name in self._device_data_pointer
-            assert name in self._host_data
-
-            device_array_ptr = self._device_data_pointer[name]
-            pycuda_driver.memcpy_htod(device_array_ptr, self._host_data[name])
-        else:
-            for key, host_array in self._host_data.items():
-                device_array_ptr = self._device_data_pointer[key]
-                pycuda_driver.memcpy_htod(device_array_ptr, host_array)
-                
-    def _to_device(
-        self,
-        name: str,
-        name_on_device: Optional[str] = None,
-        torch_accessible: bool = False,
-    ):
-        assert name in self._host_data
-        host_array = self._host_data[name]
-        if name_on_device is None:
-            name_on_device = name
-        assert name_on_device not in self._device_data_pointer
-        if not torch_accessible:
-            device_array_ptr = pycuda_driver.mem_alloc(host_array.nbytes)
-            pycuda_driver.memcpy_htod(device_array_ptr, host_array)
-            self._device_data_pointer[name_on_device] = device_array_ptr
-        else:
-            torch_tensor_device = torch.from_numpy(host_array).cuda()
-            self._device_data_via_torch[name_on_device] = torch_tensor_device
-            self._device_data_pointer[name_on_device] = CudaTensorHolder(
-                torch_tensor_device
-            )
---
+++ warp_drive/managers/pycuda/pycuda_data_manager.py
@@ -0,0 +1,77 @@
+# Copyright (c) 2021, salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root
+# or https://opensource.org/licenses/BSD-3-Clause
+
+
+from typing import Optional
+
+import numpy as np
+import pycuda.autoinit
+import torch
+
+import pycuda.driver as pycuda_driver
+
+from warp_drive.managers.data_manager import CUDADataManager
+
+class CudaTensorHolder(pycuda_driver.PointerHolderBase):
+    """
+    A class that facilitates casting tensors to pointers.
+    """
+
+    def __init__(self, t):
+        super().__init__()
+        self.gpudata = t.data_ptr()
+
+
+class PyCUDADataManager(CUDADataManager):
+    
+    def pull_data_from_device(self, name: str):
+        
+        assert name in self._host_data
+        if name in self._scalar_data_list:
+            return self._host_data[name]
+
+        if self.is_data_on_device_via_torch(name):
+            return self._device_data_via_torch[name].cpu().numpy()
+
+        assert name in self._device_data_pointer
+
+        v = np.empty_like(self._host_data[name])
+        pycuda_driver.memcpy_dtoh(v, self._device_data_pointer[name])
+        return v
+
+    def reset_device(self, name: Optional[str] = None):
+        if name is not None:
+            assert name in self._device_data_pointer
+            assert name in self._host_data
+
+            device_array_ptr = self._device_data_pointer[name]
+            pycuda_driver.memcpy_htod(device_array_ptr, self._host_data[name])
+        else:
+            for key, host_array in self._host_data.items():
+                device_array_ptr = self._device_data_pointer[key]
+                pycuda_driver.memcpy_htod(device_array_ptr, host_array)
+                
+    def _to_device(
+        self,
+        name: str,
+        name_on_device: Optional[str] = None,
+        torch_accessible: bool = False,
+    ):
+        assert name in self._host_data
+        host_array = self._host_data[name]
+        if name_on_device is None:
+            name_on_device = name
+        assert name_on_device not in self._device_data_pointer
+        if not torch_accessible:
+            device_array_ptr = pycuda_driver.mem_alloc(host_array.nbytes)
+            pycuda_driver.memcpy_htod(device_array_ptr, host_array)
+            self._device_data_pointer[name_on_device] = device_array_ptr
+        else:
+            torch_tensor_device = torch.from_numpy(host_array).cuda()
+            self._device_data_via_torch[name_on_device] = torch_tensor_device
+            self._device_data_pointer[name_on_device] = CudaTensorHolder(
+                torch_tensor_device
+            )

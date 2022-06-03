e6c3ce033d351f92e898446c9405723319228d6a
Tian Lan <tian.lan@tianlan-ltm.internal.salesforce.com>
2022-06-03 15:25:54 -0700

    temporarily rm __version__

--- warp_drive/__init__.py
+++ warp_drive/__init__.py
@@ -4,11 +4,3 @@
 # For full license text, see the LICENSE file in the repo root
 # or https://opensource.org/licenses/BSD-3-Clause
 
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    _dist = get_distribution("rl-warp-drive")
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version

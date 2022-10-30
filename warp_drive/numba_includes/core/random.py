d7e21ff6fe1101ce78758101fe5c5fd2ff80c4df
Tian Lan <tian.lan@salesforce.com>
2022-10-30 12:01:08 -0700

    add function types signature

--- warp_drive/numba_includes/core/random.py
+++ warp_drive/numba_includes/core/random.py
@@ -1,12 +1,13 @@
 from numba import cuda as numba_driver
-from numba import float32, int32
+from numba import float32, int32, from_dtype
 from numba.cuda.random import init_xoroshiro128p_states, xoroshiro128p_uniform_float32
+import numpy as np
 
 kEps = 1.0e-8
+xoroshiro128p_type = from_dtype(np.dtype([("s0", np.uint64), ("s1", np.uint64)], align=True))
 
 
-# @numba_driver.jit(int32(float32[::1], float32, int32, int32), device=True)
-@numba_driver.jit(device=True, inline=True)
+@numba_driver.jit(int32(float32[:, :, :], float32, int32, int32, int32), device=True, inline=True)
 def search_index(distr, p, env_id, agent_id, r):
     left = 0
     right = r
@@ -29,8 +30,7 @@ def init_random(rng_states, seed):
     init_xoroshiro128p_states(states=rng_states, seed=seed)
 
 
-# @numba_driver.jit((float32[::1], int32[::1], float32[::1], int32, int32))
-@numba_driver.jit
+@numba_driver.jit((xoroshiro128p_type[:], float32[:, :, :], int32[:, :], float32[:, :, :], int32))
 def sample_actions(rng_states, distr, action_indices, cum_distr, num_actions):
     env_id = numba_driver.blockIdx.x
     # Block id in a 1D grid
